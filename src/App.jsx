91c32784253f5314c58c9407189df9d855c40dad
Luka Popovic <lukapopovic0111@gmail.com>
2025-10-15 22:09:08 +0200

    Edit form style in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <form className="search-form">
+        <input
+          type="text"
+          className="text-input"
+          id="text-input"
+          name="text-input"
+        />
+        <button type="submit" className="search-btn">
+          Search
+        </button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
